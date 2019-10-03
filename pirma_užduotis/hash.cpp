65e9879db6f54dae0a443be049a6174862185293
Simonas <simonas@Pauliuss-iMac.local>
2019-10-03 17:21:56 +0300

    is this is even working

--- pirma_užduotis/hash.cpp
+++ pirma_užduotis/hash.cpp
@@ -2,18 +2,24 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector> 
 
 
-using std::string, std::cout,std::cin,std::endl,std::ostringstream,std::hex,std::uppercase,std::stringstream;
+using std::string, std::cout,std::cin,std::endl,std::ostringstream,std::hex,std::uppercase,std::stringstream,std::vector;
 
 //returns 64 length alphanumeric string
 string hashFunction(string input){
 
-    // int seed[] = {};
+    // converts a string to numeric values
+    vector <string> subNumbers;
     string seedString;
-    for(int i =0; i< input.length(); i++){
+
+    for(int i = 0; i< input.length(); i++){
+        //numeric value of character
         int temp =  static_cast<int>(input[i]);
+        //numric value converted to string
         string tempString = std::to_string(temp);
+        //Adds 0's for uniques
         if(i != 0 && tempString.length() < 3){
             string sub = "";
             for (int j = 0; j < 3-tempString.length(); j++){
@@ -22,19 +28,32 @@ string hashFunction(string input){
             tempString = sub + tempString;
         }
         seedString += tempString;
+        //Every 18 digits create new number
+        if(i % 6 == 0){
+            subNumbers.push_back(seedString);
+            seedString = "";
+        }
     }
-    stringstream ss(seedString); 
-    long long int seed = 0;
 
-    ss >> seed;
-    cout << seed << endl;
+    vector<int> seedVector;
+    //convert a string of int to int
+    for(int i = 0; i < subNumbers.size(); i++){
+        stringstream ss(subNumbers.at(i)); 
+        long long int seed;
+        ss >> seed;
+        seedVector.push_back(seed);
+        cout << subNumbers.at(i) << endl;
+        cout << seedVector.at(i) << endl;
+    }
 
     string optionArray = "12345678890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
     string output = "";
 
-    srand(seed);
-    for(int i = 0; i < 64; i++){
-        int random = rand() % 64;
+
+    srand(seedVector[0]);
+    for(int i = 0; i < 200; i++){
+        int random = rand() % optionArray.length();
+        int randomSeed = rand() % seedVector.size();
         output += optionArray[random];
     }
     return output;
@@ -49,17 +68,13 @@ bool checkPrime(int number){
     return false;
 }
 
-
-
-
 int main()
 {
+    std::vector <string> colour; 
     string input = ""; 
     cout << "Enter a string to hash " <<endl;
     cin >> input;
-
     cout << hashFunction(input) << endl;
-    // hashFunction(input);
     return 0;
 }
 
