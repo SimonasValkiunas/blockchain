25a0697351407f23013071ce1a484734caee61cf
Simonas <simonas@Pauliuss-iMac.local>
2019-10-04 16:50:48 +0300

    redoing all the code

--- pirma_užduotis/hash.cpp
+++ pirma_užduotis/hash.cpp
@@ -7,13 +7,19 @@
 
 using std::string, std::cout,std::cin,std::endl,std::ostringstream,std::hex,std::uppercase,std::stringstream,std::vector;
 
-//returns 64 length alphanumeric string
-string hashFunction(string input){
+int * generateRow(int seed,int size, int upperbound){
+    int output[size];
+    int * pointer = output;
+    srand(seed);
+    for(int i = 0; i < size; i++){
+        output[i] = rand() % upperbound;
+    }
+    return pointer;
+}
 
-    // converts a string to numeric values
-    vector <string> subNumbers;
-    string seedString;
 
+string getUniqueNumber(string input){
+    string output = "";
     for(int i = 0; i< input.length(); i++){
         //numeric value of character
         int temp =  static_cast<int>(input[i]);
@@ -25,47 +31,97 @@ string hashFunction(string input){
             for (int j = 0; j < 3-tempString.length(); j++){
                 sub += "0"; 
             }
-            tempString = sub + tempString;
+        tempString = sub + tempString;
         }
-        seedString += tempString;
-        //Every 18 digits create new number
-        if(i % 6 == 0){
-            subNumbers.push_back(seedString);
-            seedString = "";
-        }
-    }
-
-    vector<int> seedVector;
-    //convert a string of int to int
-    for(int i = 0; i < subNumbers.size(); i++){
-        stringstream ss(subNumbers.at(i)); 
-        long long int seed;
-        ss >> seed;
-        seedVector.push_back(seed);
-        cout << subNumbers.at(i) << endl;
-        cout << seedVector.at(i) << endl;
-    }
-
-    string optionArray = "12345678890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-    string output = "";
-
-
-    srand(seedVector[0]);
-    for(int i = 0; i < 200; i++){
-        int random = rand() % optionArray.length();
-        int randomSeed = rand() % seedVector.size();
-        output += optionArray[random];
+        output += tempString;
     }
     return output;
 }
 
-bool checkPrime(int number){
-    for (int i =2; i <= 128; i++){
-        if(number % i == 0){
-            return true;
+vector<unsigned long long int> divideIntoSubs(string seedString,int size){
+    vector<unsigned long long int> output;
+    if(seedString.length() <= (size/3) ){
+        unsigned long long int seed;
+        stringstream ss(seedString); 
+        ss >> seed;
+        output.push_back(seed);
+        cout << "Not divided";
+        return output;
+    }else {
+        string temp = "";
+        int divider = seedString.length() / 16;
+        int remainder = seedString.length() % 16;
+        for(int i = 0; i < seedString.length(); i++){
+            temp += seedString[i];
+            if(i!=0 && i % size == 0){
+                unsigned long long int seed;
+                stringstream ss(temp); 
+                ss >> seed;
+                output.push_back(seed);
+                temp = "";
+            }
         }
+        cout << "Divided"<<endl;
+        return output;
     }
-    return false;
+}
+
+
+
+
+
+//returns 64 length alphanumeric string
+string hashFunction(string input){
+
+    string unique = getUniqueNumber(input);
+    vector<unsigned long long int> subNumbers = divideIntoSubs(unique,16);
+
+    for(vector<unsigned long long int>::iterator it = subNumbers.begin(); it != subNumbers.end(); ++it){
+        cout << *it << endl;
+        for(int i = 0; i < 30; i++){
+            cout << *(generateRow(*it,30,64)+i) << " ";
+        }
+        cout << endl;
+    }
+
+
+    // // ----------------------------------
+
+    // //Declaring seedVector and seed int
+    // vector<int> seedVector;
+    // long long int seed;
+
+    // //Check if seed is long
+    // if (subNumbers.size() > 1){
+    //     //convert a string of int to int
+    //     for(int i = 0; i < subNumbers.size(); i++){
+    //         stringstream ss(subNumbers.at(i)); 
+    //         ss >> seed;
+    //         seedVector.push_back(seed);
+    //         //use first string as seed
+    //     }
+    //     srand(seedVector[0]);
+    //     cout << seedVector[0] << endl;
+    // }else {
+    //     //use whole string as seed
+    //     stringstream ss(seedString); 
+    //     ss >> seed;
+    //     srand(seed);
+    // }
+
+    // // ------------------------------------
+
+    // string optionArray = "12345678890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    // string output = "";
+
+
+
+    // for(int i = 0; i < 100; i++){
+    //     int random = rand() % optionArray.length();
+    //     output += optionArray[random];
+    // }
+    string output = "";
+    return output;
 }
 
 int main()
