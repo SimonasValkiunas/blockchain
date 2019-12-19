30a421b9b1a3763d086efff4ae7235313357be0b
Simonas <simonas.valkiunas@gmail.com>
2019-12-19 22:00:24 +0200

    last day commits - revised antra_uzduotis

--- antra_uzduotis/app.js
+++ antra_uzduotis/app.js
@@ -115,10 +115,9 @@ class User{
 }
 
 class Block{
-    constructor(prev_block,timestamp, merkel_hash,version, nonce, dificulty_target,transactions = []){
+    constructor(prev_block,timestamp,version, nonce, dificulty_target,transactions = []){
         this.header = {
 			prev_block : prev_block,
-			merkel_hash : merkel_hash,
             timestamp : timestamp,
             version : version,
             nonce : nonce,
@@ -130,11 +129,43 @@ class Block{
     }
 
     hashBlock(){
-        return sha256(this.header.prev_block + this.header.timestamp + this.header.version + this.header.nonce + this.header.dificulty_target + JSON.stringify(this.body));
+        return sha256(this.header.prev_block + this.header.timestamp + this.header.version + this.header.nonce + this.header.dificulty_target);
 	}
 	
 	iterate_nonce(){
 		this.header.nonce += 1;
+	}
+
+	merkel_hash(lowestLevel = JSON.parse(JSON.stringify(this.body.transactions))){
+		let newLevel = [];
+
+		if(lowestLevel.length == 0) {
+			this.merkel = sha256(JSON.stringify({}));
+			return this.merkel;
+
+		}
+
+		if(lowestLevel.length == 1) {
+			this.merkel = lowestLevel[0];
+			return this.merkel;
+		};
+
+		if(lowestLevel.length % 2 == 0){
+			while(lowestLevel.length){
+				newLevel.push(sha256(JSON.stringify(lowestLevel[0])+JSON.stringify(lowestLevel[1])));
+				lowestLevel.splice(0,2);
+			}
+			return this.merkel_hash(newLevel);
+
+
+		}else if (lowestLevel.length % 2 > 0){
+			while(lowestLevel.length > 1){
+				newLevel.push(sha256(JSON.stringify(lowestLevel[0])+JSON.stringify(lowestLevel[1])));
+				lowestLevel.splice(0,2);
+			}
+			newLevel.push(sha256(JSON.stringify(lowestLevel[0])));
+			return this.merkel_hash(newLevel);
+		}
 	}
 
 }
@@ -144,22 +175,24 @@ class Transaction{
         this.from_user = from_user;
         this.to_user = to_user;
 		this.amount = amount;
-		this.ID = this.hashTransaction();
+		this.hash = this.hashTransaction();
     }
 
     hashTransaction(){
-        return sha256(JSON.stringify(this.to_user, this.from_user, this.amount));
+        return sha256(JSON.stringify(this.to_user,this.from_user,this.amount));
     }
 }
 
 
+
+// Put merkel has in block class
 class Blockchain{
 	constructor(userCount, transactionCount, version, diff_level){
 		this.version = version;
 		this.dificulty_target = diff_level;
+		let genesisBlock = new Block(null,Date.now(),this.version,0,this.dificulty_target);
+		genesisBlock.merkel_hash();
 		this.chain = {};
-		this.merkel_hash();
-		let genesisBlock = new Block(null,Date.now(),this.merkel,this.version,0,this.dificulty_target);
 		this.chain[genesisBlock.hashBlock()] = genesisBlock;
 		this.users = this.generateUsers(userCount);
 		this.transactions = this.generateTransactions(transactionCount);
@@ -208,14 +241,16 @@ class Blockchain{
 
 	addBlock(size,blockSampleSize = 5){
 		for(let i =0; i < blockSampleSize; i++){
-			this.merkel_hash();
-			let block = new Block(this.chain[Object.keys(this.chain)[Object.keys(this.chain).length-1]].hashBlock(),Date.now(),this.merkel,this.version,0,this.dificulty_target);
+
+			let block = new Block(this.chain[Object.keys(this.chain)[Object.keys(this.chain).length-1]].hashBlock(),Date.now(),this.version,0,this.dificulty_target);
 			for(let i = 0; i < size; i++){
 				let random = Math.floor(Math.random()*this.transactions.length);
-				if(this.transactions[random].ID === this.transactions[random].hashTransaction()){
+				
+				if(this.transactions[random].hash === this.transactions[random].hashTransaction()){
 					block.body.transactions.push(this.transactions[random]);
 				}
 			}
+			block.merkel_hash();
 			this.nextBlocks.push(block);
 		}
 	}
@@ -236,44 +271,10 @@ class Blockchain{
 		}
 		console.log(`Transactions removed: ${transCount-this.transactions.length}`);
 	}
-
-	merkel_hash(lowestLevel = Object.keys(this.chain)){
-
-		let newLevel = [];
-		let int = lowestLevel.length;
-		if(int == 0) {
-			return sha256(JSON.stringify({}));
-		}
-
-		if(int == 1) {
-			this.merkel = lowestLevel[0];
-			return;
-		};
-
-		if(int % 2 == 0){
-			while(lowestLevel.length){
-				newLevel.push(sha256(lowestLevel[0]+lowestLevel[1]));
-				lowestLevel.splice(0,2);
-			}
-			this.merkel_hash(newLevel);
-
-
-		}else if (int % 2 > 0){
-			while(lowestLevel.length > 1){
-				newLevel.push(sha256(lowestLevel[0]+lowestLevel[1]));
-				lowestLevel.splice(0,2);
-			}
-			newLevel.push(sha256(lowestLevel[0]));
-			this.merkel_hash(newLevel);
-		}
-	}
-
-
 }
 
 
 let myChain = new Blockchain(1000,1000,'0.1',1);
 myChain.verifyTransactions();
 myChain.populateChain(100);
-// myChain.merkel_hash()
-console.log(myChain.chain[Object.keys(myChain.chain)[7]].header)
+console.log(myChain);
